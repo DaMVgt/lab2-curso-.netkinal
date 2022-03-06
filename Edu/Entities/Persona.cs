aad6ff99aca5928bbf36969a5ddc9d1d2c8c6a36
DaMVgt <dmonterroso098@outlook.com>
2022-03-06 12:23:25 -0600

    Iniciando laborotario 2, agregado carpetas Entities e Interfaces, creacion de clase padre Persona

---
+++ Edu/Entities/Persona.cs
@@ -0,0 +1,15 @@
+namespace AsignacionLab2.Edu.Entities
+{
+    public class Persona 
+    {
+        public string Uuid {get;set;}
+        public string Apellidos {get;set;}
+        public string Nombres {get;set;}
+        public string Email {get;set;}
+
+        public void TomarAsistencia()
+        {
+            
+        }
+    }
+}
