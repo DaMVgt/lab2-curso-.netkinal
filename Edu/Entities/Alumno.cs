132b7c4a6ed28e48dfda608ecf29483a9b30919e
DaMVgt <dmonterroso098@outlook.com>
2022-03-06 12:35:38 -0600

    Agregado clases hijas Alumno y Profesor, se reseteo formato en clase padre Persona

---
+++ Edu/Entities/Alumno.cs
@@ -0,0 +1,9 @@
+namespace AsignacionLab2.Edu.Entities
+{
+    public class Alumno : Persona
+    {
+        public string Carne { get; set; }
+        public string NumeroCreditos { get; set; }
+
+    }
+}
---
+++ Edu/Entities/Profesor.cs
@@ -0,0 +1,8 @@
+namespace AsignacionLab2.Edu.Entities
+{
+    public class Profesor
+    {
+        public string Cui { get; set; }
+        public string Cargo { get; set; }
+    }
+}
